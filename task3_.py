43a564ad2d4753b1531b8ced9968f8276a0b1c22
Ming Yi <myi@student.ethz.ch>
2019-11-30 16:08:30 +0100

    grid search, get best params gamma 0.02, C 10

--- task3_.py
+++ task3_.py
@@ -10,11 +10,13 @@ from sklearn.model_selection import cross_val_score
 # from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import balanced_accuracy_score, make_scorer
 from sklearn.metrics import f1_score
-from neuralNet import neurNet_classifier
+# from neuralNet import neurNet_classifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.impute import SimpleImputer
 from pyhrv import hrv
+import pyhrv.time_domain as td
+from sklearn.model_selection import GridSearchCV
 
 
 def read_from_file(X_train_file, y_train_file, X_predict_file):
@@ -45,11 +47,12 @@ def feature_extraction(X):
         template_median = np.median(templates, axis=0)
         # take the minimum R peaks
         rpeaks_location = signal_processed[2]
-        rpeaks_location = ecg.correct_rpeaks(signals = row, rpeaks = rpeaks_location, sampling_rate=300)
+        rpeaks_location = ecg.correct_rpeaks(signal = row, rpeaks = rpeaks_location, sampling_rate=300)
         rpeaks = row[rpeaks_location]
         rpeaks_min = min(rpeaks)
         rpeaks_max = max(rpeaks)
-
+        rpeaks_mean = np.mean(rpeaks)
+        rpeaks_var = np.var(rpeaks)
         # take the hearbeat rate
         # heartbeat_rate = signal_processed[-1]
         # in case where the heartbeat is empty in the result
@@ -59,20 +62,26 @@ def feature_extraction(X):
         # else:
         #     hb_rate_min = min(heartbeat_rate)
         #     hb_rate_max = max(heartbeat_rate)
-
-        # add RR intervals into the feature
+        # add RR intervals var into the feature
         rr_interval = np.diff(rpeaks_location)
-        print("rr_interval: ", rr_interval)
-        rr_diff = max(rr_interval) - min(rr_interval)
-        features = np.append(template_median, [rpeaks_min, rpeaks_max, rr_diff])
-
+        # print("rr_interval: ", rr_interval)
+        rr_var = np.var(rr_interval)
+        rr_min = np.min(rr_interval)
+        rr_max = np.max(rr_interval)
+        # add hrv into the feature
+        # hrv_val = caculate_hrv(row, rpeaks)
+        features = np.append(template_median, [rpeaks_min, rpeaks_max, rpeaks_mean, rpeaks_var, rr_min, rr_max, rr_var])
         # add the new point into  all datapoints
         X_new.append(features)
     X_new = np.array(X_new)
     return X_new
 
 
-def caculae_hrv(signal, rpeaks_time, ):
+# def caculate_hrv(signal_row, rpeaks):
+#     # caculate heartbeat variance
+#     nni_param = td.nni_parameters(rpeaks=rpeaks)
+#     hrv_val = hrv(nni = nni_param, signal = signal_row, sampling_rate=300)['hr_std']
+#     return hrv_val
 
 
 def processed_to_csv(X_train, flag = 'train'):
@@ -102,28 +111,39 @@ def standarlization(train_x, test_x):
 
 def svmClassifier(train_x, train_y, test_x):
     train_y = train_y.ravel()
-    classifier = SVC(class_weight='balanced', gamma=0.005, C=1)  # c the penalty term for misclassification
+    classifier = SVC(class_weight='balanced', gamma=0.02, C=10)  # c the penalty term for misclassification
     # make balanced_accuracy_scorer
     score_func = make_scorer(f1_score, average='micro') # additional param for f1_score
     # cross validation
     scores = cross_val_score(classifier, train_x, train_y, cv=5, scoring=score_func)
     print(scores)
-
     # learn on all data
     classifier.fit(train_x, train_y)
     y_predict_test = classifier.predict(test_x)
     return y_predict_test
 
 
+def grid_search(train_x, train_y, test_x):
+    parameters = {'C': [0.5, 1, 5, 10], 'gamma': [0.005, 0.01, 0.02, 0.05, 0.1]}
+    svcClassifier = SVC(kernel='rbf', class_weight='balanced')
+    score_func = make_scorer(f1_score, average='micro')
+    gs = GridSearchCV(svcClassifier, parameters, cv=5, scoring=score_func)
+    gs.fit(train_x, train_y)
+    print(gs.cv_results_)
+    print(gs.best_params_)
+    print(gs.best_score_)
+    y_predict_test = gs.predict(test_x)
+    return y_predict_test
+
+
 def adaBoostClassifier(train_x, train_y, test_x):
     train_y = train_y.ravel()
-    classifier = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=1), n_estimators=55, learning_rate=1)
+    classifier = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=3, class_weight='balanced'), n_estimators=50, learning_rate=1)
     # make balanced_accuracy_scorer
     score_func = make_scorer(f1_score, average='micro')  # additional param for f1_score
     # cross validation
     scores = cross_val_score(classifier, train_x, train_y, cv=5, scoring=score_func)
     print(scores)
-
     # learn on all data
     classifier.fit(train_x, train_y)
     y_predict_test = classifier.predict(test_x)
@@ -147,7 +167,6 @@ if __name__ == '__main__':
         x_std = standarlization(x_train_temMed, x_test_temMed)
         x_train_std = x_std[0]
         x_test_std = x_std[1]
-
         # write processed data to csv
         processed_to_csv(x_train_std)
         processed_to_csv(x_test_std,flag = 'test')
@@ -158,12 +177,13 @@ if __name__ == '__main__':
         y_train = pd.read_csv('y_train.csv', index_col='id').to_numpy()
         # print(x_train_std[[10, 14, 17, 18]][:, -2:])
     # prediction
-    # y_predict = grid_search(x_train_selected, y_train, x_test_selected)
+    # y_predict = grid_search(x_train_std, y_train, x_test_std)
     y_predict = svmClassifier(x_train_std, y_train, x_test_std)
     # neural net
     # y_predict = neurNet_classifier(x_train_std, y_train, x_test_std)
     # Adaboost classifier
     # y_predict = adaBoostClassifier(x_train_std, y_train, x_test_std)
+    # grid search
     result_to_csv(y_predict, 'sample.csv')
 
 
